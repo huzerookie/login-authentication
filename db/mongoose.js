c7a280cf305be1746518d56c10ba8b4650a016b6
Huzefa <ultimatehuzefa@gmail.com>
2021-03-23 03:52:24 +0530

    db issue

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-const mongooseURL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/login-authentication-api';
-
+//const mongooseURL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/login-authentication-api';
+const mongooseURL = 'mongodb+srv://huzerookie:A$$h0les@cluster0.fuaif.mongodb.net/login-authentication-api?retryWrites=true&w=majority'
 //Connection Setup for Mongoose
+console.log(mongooseURL)
 mongoose.connect(mongooseURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
